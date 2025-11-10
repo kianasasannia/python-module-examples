66f7f084ad934f30195931a9a1f696f7b0eaece7
Kiana <97636585+kianasasannia@users.noreply.github.com>
2025-11-10 23:38:55 +0330

    4-pyfigletmodule.py

---
+++ 4-pyfigletmodule.py
@@ -0,0 +1,16 @@
+import pyfiglet
+from termcolor import colored
+
+# result = pyfiglet.figlet_format('Mark')
+# print(result)
+
+# r= pyfiglet.figlet_format('Alice', font = 'letters')
+# print(r)
+
+# f=pyfiglet.FigletFont.getFonts()
+# print(f)
+
+# r2 = colored(pyfiglet.figlet_format('Welcome', font = 'banner3-D'), 'red')
+# print(r2)
+
+
